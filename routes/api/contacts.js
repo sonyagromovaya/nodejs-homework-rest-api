0cc3df0ceb1e79aa0557288cfa1f468d75aaa513
Sofia Gromovaya <sonya.gromovaya2004@gmail.com>
2023-01-29 12:29:10 +0200

    third one

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,39 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+// contacts controllers
+const ctrl = require("../../controllers/contacts");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// try-catch wrapper
+const { ctrlWrapper } = require("../../helpers");
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// validate request body and ID
+const { validateBody, isValidId } = require("../../middlewares");
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// Joi validate Schema
+const { joiSchema } = require("../../models/contact");
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = express.Router();
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", ctrlWrapper(ctrl.getAll));
 
-module.exports = router
+router.get("/:contactId", isValidId, ctrlWrapper(ctrl.getById));
+
+router.post("/", validateBody(joiSchema.addSchema), ctrlWrapper(ctrl.add));
+
+router.put(
+  "/:contactId",
+  isValidId,
+  validateBody(joiSchema.addSchema),
+  ctrlWrapper(ctrl.updateById)
+);
+
+router.patch(
+  "/:contactId/favorite",
+  isValidId,
+  validateBody(joiSchema.updateFavoriteSchema),
+  ctrlWrapper(ctrl.updateStatusContact)
+);
+
+router.delete("/:contactId", ctrlWrapper(ctrl.removeById));
+
+module.exports = router;
